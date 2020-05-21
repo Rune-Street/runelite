8ac6bf20ec82b516fe89516fdd1568d845552362
Clement Liaw <cklliaw@uwaterloo.ca>
2020-05-21 10:30:50 -0400

    Changed rebase.sh to not automatically push after rebasing [ci skip]

--- rebase.sh
+++ rebase.sh
@@ -4,8 +4,8 @@ cd "${0%/*}"
 git fetch upstream
 git checkout master
 git rebase upstream/master
-git push -f
+#git push -f
 git checkout rune-street
 git rebase master
 git commit --allow-empty -m "Rebase"
-git push -f
+#git push -f
